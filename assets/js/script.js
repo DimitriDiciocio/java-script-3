c1627f6a1677220ad49cf6b1ffd1e99abeec88d5
DimitriDiciocio <dimitric2007@gmail.com>
2024-09-04 21:00:35 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -478,13 +478,11 @@ function suporte() {
 //#region 
     //24
     function shotDados() {
-        let count = 0
         let dados = []
         for (i=1;i<101;i++) {
             let dado = randomIntFromInterval(1,6)
             dados.push(dado)
         }
-        let database = []
         alert("Quantidade de dados em 100 lançamentos:\n\n1: " + dados.filter(x => x==1).length + "\n2: " + dados.filter(x => x==2).length + "\n3: " + dados.filter(x => x==3).length + "\n4: " + dados.filter(x => x==4).length + "\n5: " + dados.filter(x => x==5).length + "\n6: " + dados.filter(x => x==6).length)
         
     }
